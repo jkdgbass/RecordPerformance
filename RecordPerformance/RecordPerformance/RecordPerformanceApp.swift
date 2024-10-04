d9428e576a7c32e75ce942e95add99f90c50cdc3
김도형 <jkdgbass@gmail.com>
2024-10-04 15:57:09 +0900

    프로젝트 생성 commit

---
+++ RecordPerformance/RecordPerformance/RecordPerformanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecordPerformanceApp.swift
+//  RecordPerformance
+//
+//  Created by DoHyoung Kim on 10/4/24.
+//
+
+import SwiftUI
+
+@main
+struct RecordPerformanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
