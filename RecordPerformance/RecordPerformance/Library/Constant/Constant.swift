168650b093c1b8d1064c4887beac6b8245540d17
김도형 <jkdgbass@gmail.com>
2024-10-22 20:08:31 +0900

    Font_Extensions를 없애고 Constant로 코드 옮김

--- RecordPerformance/RecordPerformance/Library/Constant/Constant.swift
+++ RecordPerformance/RecordPerformance/Library/Constant/Constant.swift
@@ -15,3 +15,19 @@ class Constant {
     let user = UserConstant()
 }
 
+//Font size
+let headLine1: CGFloat = 28
+let title1: CGFloat = 24
+let title2: CGFloat = 22
+let body1: CGFloat = 16
+let body2: CGFloat = 14
+let body3: CGFloat = 12
+let caption2: CGFloat = 11
+
+//Radius size
+let radiusXl: CGFloat = 2
+let radiusLg: CGFloat = 4
+let radiusMd: CGFloat = 8
+let radiusSm: CGFloat = 12
+let radiusXs: CGFloat = 16
+let radius2Xs: CGFloat = 24
