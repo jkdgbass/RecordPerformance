12e414349400be48a5d04d92fbad5257b3ee8dd0
김도형 <jkdgbass@gmail.com>
2024-10-29 14:49:14 +0900

    레코드 메인 화면에 더하기 object 추가 및 safe area top 추가

--- RecordPerformance/RecordPerformance/UI/Main/RecordMainView.swift
+++ RecordPerformance/RecordPerformance/UI/Main/RecordMainView.swift
@@ -11,25 +11,39 @@ struct RecordMainView: View {
     @StateObject var model = RecordMainViewModel()
     
     var body: some View {
-        VStack(spacing: 0) {
-            NavigationView(name: "Record")
-            
-            MainCalendar(currentDate: $model.currentDate)
-                .padding(.all, 16)
-            
-            ScrollView {
-                LazyVStack {
-                    ForEach(1...10, id: \.self) { count in
-                        RecordRowView()
-                            .padding(.bottom, 32)
+        ZStack(alignment: .bottomTrailing) {
+            VStack(spacing: 0) {
+                NavigationView(name: "Record")
+                    .padding(.top, UIScreen.topSafeArea)
+                
+                MainCalendar(currentDate: $model.currentDate)
+                    .padding(.all, 16)
+                
+                ScrollView {
+                    LazyVStack {
+                        ForEach(1...10, id: \.self) { count in
+                            RecordRowView()
+                                .padding(.bottom, 32)
+                        }
                     }
                 }
+                .foregroundStyle(Color.surfaceContainerLow)
+                .cornerRadius(radiusXl, corners: [.topLeft, .topRight])
             }
-            .foregroundStyle(Color.surfaceContainerLow)
-            .cornerRadius(radiusXl, corners: [.topLeft, .topRight])
-        }
-        .background {
-            Color.black
+            
+            Button {
+                
+            } label: {
+                RoundedRectangle(cornerRadius: radiusSm)
+                    .overlay(content: {
+                        Image("ico_charge")
+                    })
+                    .foregroundStyle(Color.surfaceBright)
+                    .frame(width: 48, height: 48)
+            }
+            .padding(.trailing, 16)
+            .padding(.bottom, 16)
+
         }
         .ignoresSafeArea(.all)
     }
